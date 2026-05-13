d139bec626e43e14ce73b1ed06847b170b7006f3
Abd AL Rahman Miassar <abodimiassar@gmail.com>
2026-05-13 19:28:46 +0300

    Add initial Slug Generator API controllers and services

---
+++ SlugGenerator.Api/SlugGenerator.Core/SlugGeneratorService.cs
@@ -0,0 +1,27 @@
+﻿namespace SlugGenerator.Core
+{
+    public class SlugGeneratorService : ISlugGeneratorService
+    {
+        public string Generate(string text, char separator = '-')
+        {
+            ArgumentNullException.ThrowIfNull(text);
+
+            return string.Join(separator, text
+            .Where(ch => char.IsLetter(ch) || ch == ' ' || ch == separator)
+            .ToArray()
+            .AsMemory()
+            .ToString()
+            .ToLowerInvariant()
+            .Split(new[] { ' ', separator }, StringSplitOptions.RemoveEmptyEntries)
+);
+        }
+
+        public string GenerateUnique(string text)
+        {
+            ArgumentNullException.ThrowIfNull(text);
+
+            System.Guid uniqueId = System.Guid.NewGuid();
+            return $"{Generate(text)}-{uniqueId}";
+        }
+    }
+}
