e38b5a1aa90f05805908a3ddde6a71f34524e8da
Abd AL Rahman Miassar <abodimiassar@gmail.com>
2026-05-21 14:00:24 +0300

    feat: add global error handling middleware

---
+++ SlugGenerator.Api/SlugGenerator.Api/Middlewares/GlobalExceptionHandler.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SlugGenerator.Api.Infrastructure;
+
+public class GlobalExceptionHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+        var problemDetails = new ProblemDetails
+        {
+            Status = StatusCodes.Status500InternalServerError, 
+            Title = "Server Error", 
+
+            Detail = "An unexpected error occurred while processing your request. Please try again later."
+        };
+
+        httpContext.Response.StatusCode = problemDetails.Status.Value;
+        httpContext.Response.ContentType = "application/problem+json";
+
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
+}
