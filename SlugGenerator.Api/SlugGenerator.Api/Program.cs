9cdf9a324a5131a8e98dda4d636808be648c118c
Abd AL Rahman Miassar <abodimiassar@gmail.com>
2026-05-23 17:10:32 +0300

    fea :Add FluentValidation

--- SlugGenerator.Api/SlugGenerator.Api/Program.cs
+++ SlugGenerator.Api/SlugGenerator.Api/Program.cs
@@ -1,18 +1,31 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Add the SlugGeneratorService to the dependency injection container
 builder.Services.AddScoped<SlugGenerator.Core.ISlugGeneratorService, SlugGenerator.Core.SlugGeneratorService>();
+
+
+// Add FluentValidation services
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblyContaining<SlugGenerator.Api.Validators.SlugRequestValidator>();
+
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Add ProblemDetails and global exception handling
 builder.Services.AddProblemDetails();
-
 builder.Services.AddExceptionHandler<SlugGenerator.Api.Infrastructure.GlobalExceptionHandler>();
 
+
+// Add API versioning
 builder.Services.AddApiVersioning(options => {
     options.DefaultApiVersion = new Asp.Versioning.ApiVersion(1, 0);
     options.AssumeDefaultVersionWhenUnspecified = true;
@@ -22,8 +35,10 @@ builder.Services.AddApiVersioning(options => {
     options.SubstituteApiVersionInUrl = true;
 });
 
+
 var app = builder.Build();
 
+// Add global exception handling middleware
 app.UseExceptionHandler();
 
 // Configure the HTTP request pipeline.
---
+++ SlugGenerator.Api/SlugGenerator.Api/Validators/SlugRequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using SlugGenerator.Api.DTOs;
+using FluentValidation;
+
+namespace SlugGenerator.Api.Validators
+{
+    public class SlugRequestValidator : AbstractValidator<GenerateSlugRequest>
+    {
+        public SlugRequestValidator()
+        {
+            RuleFor(request => request.Text)
+            .NotEmpty()
+            .WithMessage("Text is required.")
+            .MaximumLength(500)
+            .WithMessage("Text must not exceed 500 characters.");
+
+            RuleFor(request => request.Separator)
+            .Must(separator => separator == '-' || separator == '_')
+            .WithMessage("Separator must be either '-' or '_'.");
+        }
+
+    }
+}
