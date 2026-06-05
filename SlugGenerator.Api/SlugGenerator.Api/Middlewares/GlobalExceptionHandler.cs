066bba85a0a7df78c1646b7a4c3a707b25aacdc0
Abd AL Rahman Miassar <abodimiassar@gmail.com>
2026-06-05 03:26:51 +0300

    add logging and validation.

--- SlugGenerator.Api/SlugGenerator.Api/Middlewares/GlobalExceptionHandler.cs
+++ SlugGenerator.Api/SlugGenerator.Api/Middlewares/GlobalExceptionHandler.cs
@@ -1,22 +1,43 @@
 ﻿using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using static System.Net.WebRequestMethods;
 
 namespace SlugGenerator.Api.Infrastructure;
 
 public class GlobalExceptionHandler : IExceptionHandler
 {
+    private readonly ILogger<GlobalExceptionHandler> _logger;
+    public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+    {
+        _logger = logger;
+    }
     public async ValueTask<bool> TryHandleAsync(
         HttpContext httpContext,
         Exception exception,
         CancellationToken cancellationToken)
     {
+
+        _logger.LogError(exception, "An exception occurred: {Message}", exception.Message);
+
+        var statusCode = StatusCodes.Status500InternalServerError;
+        var title = "Server Error";
+        var detail = "An unexpected error occurred while processing your request. Please try again later.";
+
+        if (exception is ArgumentException argumentException)
+        {
+            statusCode = StatusCodes.Status400BadRequest;
+            title = "Validation Error";
+            detail = argumentException.Message; 
+        }
+
         var problemDetails = new ProblemDetails
         {
-            Status = StatusCodes.Status500InternalServerError, 
-            Title = "Server Error", 
+            Status = statusCode, 
+            Title = title, 
 
-            Detail = "An unexpected error occurred while processing your request. Please try again later."
+            Detail = detail
         };
+
 
         httpContext.Response.StatusCode = problemDetails.Status.Value;
         httpContext.Response.ContentType = "application/problem+json";
